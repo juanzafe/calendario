aae333d166552f21d4dbdc5f1d7a208da52c641b
juanzafe <juanzafe@hotmail.com>
2025-10-08 13:01:43 +0200

    base de datosd de Firebase

---
+++ src/services/calendarService.ts
@@ -0,0 +1,30 @@
+
+import { doc, onSnapshot, deleteDoc, collection, setDoc } from "firebase/firestore";
+import { auth, db } from "../firebase/firebase";
+
+
+export async function saveClass(day: Date, data: { title: string; count: number }) {
+  if (!auth.currentUser) throw new Error("Usuario no autenticado");
+  const uid = auth.currentUser.uid;
+  const ref = doc(db, "users", uid, "classes", day.toISOString());
+  await setDoc(ref, { date: day.toISOString(), ...data });
+}
+
+
+export async function deleteClass(day: Date) {
+  if (!auth.currentUser) throw new Error("Usuario no autenticado");
+  const uid = auth.currentUser.uid;
+  const ref = doc(db, "users", uid, "classes", day.toISOString());
+  await deleteDoc(ref);
+}
+
+
+export function listenToClasses(callback: (data: any[]) => void) {
+  if (!auth.currentUser) throw new Error("Usuario no autenticado");
+  const uid = auth.currentUser.uid;
+  const ref = collection(db, "users", uid, "classes");
+  return onSnapshot(ref, (snapshot) => {
+    const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    callback(data);
+  });
+}
