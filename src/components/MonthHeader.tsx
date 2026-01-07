2df2c48228db9b1716f37e31976c8875a926be37
juanzafe <juanzafe@hotmail.com>
2026-01-07 11:38:10 +0100

    tr2

--- src/components/MonthHeader.tsx
+++ src/components/MonthHeader.tsx
@@ -4,7 +4,7 @@ import type { CalendarioAutoescuelaProps } from "./Month";
 export function MonthHeader(_props: CalendarioAutoescuelaProps) {
 	return (
 		<div className="w-full grid grid-cols-7 bg-gray-100 text-gray-700 text-xs sm:text-sm font-semibold uppercase border-b border-gray-300">
-			{["L", "Ma", "X", "Ju", "V", "S", "D"].map((dia) => (
+			{["L", "M", "X", "J", "V", "S", "D"].map((dia) => (
 				<DayHeader key={dia} label={dia} />
 			))}
 		</div>
